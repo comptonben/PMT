85c9879ca29b713fce78742064976de8fa961bf0
comptonben <comptonben504@gmail.com>
2020-02-11 13:46:32 -0600

    not working

--- uPy/main.py
+++ uPy/main.py
@@ -17,6 +17,7 @@ from utime import sleep
 from wifi_connect import *
 from machine import SDCard
 from uos import mount
+import pyb
 
 ap_blacklist = [b'xfinitywifi']
 
@@ -51,33 +52,44 @@ filepath = "/sdcard/data.txt"
 # instantiate GPS class
 gps = GPS()
 
-while True:
-    while not station.isconnected():
-        # @param nets: tuple of obj(ssid, bssid, channel, RSSI, authmode, hidden)
-        nets = station.scan()
+# setup GPS interrupt
+time_irq = pyb.Timer(4, freq=1, callback=gps_interrupt_handler)
 
-        # get only open nets
-        openNets = [n for n in nets if n[4] == 0]
+# while True:
+#     while not station.isconnected():
+#         # @param nets: tuple of obj(ssid, bssid, channel, RSSI, authmode, hidden)
+#         nets = station.scan()
 
-        for onet in openNets:
-            if onet[0] not in ap_blacklist:
-                # Try to connect to WiFi access point
-                apSSID = onet[0]
-                print ("Connecting to "+str(onet[0],"utf-8")+" ...\n")
-                station.connect(onet[0])
-                while not station.isconnected():
-                    sleep(0.5)
-                if station.isconnected():
-                    succesful_post = station_connected(station)
-                else:
-                    print("Unable to Connect") 
+#         # get only open nets
+#         openNets = [n for n in nets if n[4] == 0]
 
-    while station.isconnected():
-        GPSdata = gps.get_RMCdata()
-        if not (GPSdata == {}):
-            data = ','.join(list(v for v in GPSdata.values()))
-            data = data + "\n"
-            with open(filepath, "a+") as file_ptr:
-                file_ptr.write(data)
-            successful_post = post_data(data)
-        sleep(5)
+#         for onet in openNets:
+#             if onet[0] not in ap_blacklist:
+#                 # Try to connect to WiFi access point
+#                 apSSID = onet[0]
+#                 print ("Connecting to "+str(onet[0],"utf-8")+" ...\n")
+#                 station.connect(onet[0])
+#                 while not station.isconnected():
+#                     sleep(0.5)
+#                 if station.isconnected():
+#                     succesful_post = station_connected(station)
+#                 else:
+#                     print("Unable to Connect") 
+
+#     while station.isconnected():
+#         GPSdata = gps.get_RMCdata()
+#         if not (GPSdata == {}):
+#             data = ','.join(list(v for v in GPSdata.values()))
+#             data = data + "\n"
+#             with open(filepath, "a+") as file_ptr:
+#                 file_ptr.write(data)
+#             successful_post = post_data(data)
+#         sleep(5)
+
+
+def gps_interrupt_handler():
+    GPSdata = gps.get_RMCdata()
+    if not (GPSdata == {}):
+        data = ','.join(list(v for v in GPSdata.values()))
+        data = data + "\n"
+        print(GPSdata)
